a819f34926bc90ca2f2570a982dde62ebcc23991
JoseZancanaro <jose.zancanaro@edu.univali.br>
2019-05-15 15:02:16 -0300

    Add Breadcrumbs component

---
+++ src/components/Breadcrumbs/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { ColorLightGrey } from '../../styles';
+
+export const BreadcrumbsContainer = styled.section`
+  background: ${ColorLightGrey};
+  padding: 30px;
+
+  h1 {
+    margin-top: 5px;
+  }
+
+  div {
+    color: #ffffff;
+  }
+`;
