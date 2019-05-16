c1e738f9a08be0ecbb5b0eb052418d47ba16249b
JoseZancanaro <jose.zancanaro@edu.univali.br>
2019-05-16 02:13:15 -0300

    Add MoviePoster Component

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,8 @@ import Header from './Header';
 import Footer from './Footer';
 import SocialIcons from './SocialIcons';
 import Breadcrumbs from './Breadcrumbs';
+import MoviePoster from './MoviePoster';
 
-export { Header, Footer, SocialIcons, Breadcrumbs };
+export {
+  Header, Footer, SocialIcons, Breadcrumbs, MoviePoster,
+};
