65b3fcc4f284cbc82c23059f9abad1711cabd899
Israel Efraim de Oliveira <israel.oliveira@edu.univali.br>
2019-05-23 21:10:09 -0300

    Modularize import

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,18 +3,16 @@ import {
   BrowserRouter, Switch, Route, Redirect,
 } from 'react-router-dom';
 
-import {
-  Main, EmCartaz, EmBreve, NoMatchPage,
-} from '../pages';
+import * as Pages from '../pages';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={Main} />
+      <Route path="/" exact component={Pages.Main} />
       <Redirect from="/filmes" exact to="/filmes/em-cartaz" />
-      <Route path="/filmes/em-cartaz" component={EmCartaz} />
-      <Route path="/filmes/em-breve" component={EmBreve} />
-      <Route component={NoMatchPage} />
+      <Route path="/filmes/em-cartaz" component={Pages.SectionReleased} />
+      <Route path="/filmes/em-breve" component={Pages.SectionCommingSoon} />
+      <Route component={Pages.NoMatchPage} />
     </Switch>
   </BrowserRouter>
 );
