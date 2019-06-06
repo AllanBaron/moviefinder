6dd397b6407ed85ca84bd7101b127c82c862fd74
Israel Efraim de Oliveira <israel.oliveira@edu.univali.br>
2019-06-06 20:15:48 -0300

    Docs branch

--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Main from './Main';
-import SectionReleased from './SectionReleased';
-import SectionCommingSoon from './SectionCommingSoon';
-import NoMatchPage from './NoMatchPage';
-
-export {
-  Main, SectionReleased, SectionCommingSoon, NoMatchPage,
-};
